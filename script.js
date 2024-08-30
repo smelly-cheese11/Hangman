4425452f50dbed6fe90f41bf89b9be5e3e175203
Caleb <126200524+smelly-cheese11@users.noreply.github.com>
2024-08-30 10:59:49 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text");
+const hangmanImage = document.querySelector(".hangman-box img");
+const keyboardDiv = document.querySelector(".keyboard");
+
+let currentWord, wrongGuessCount = 0;
+const maxGuesses = 6;
+
+const getRandomWord = () => {
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    currentWord = word;
+    console.log(word, hint);
+    document.querySelector(".hint-text b").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
+}
+
+const initGame = (button, clickedLetter) => {
+    if(currentWord.includes(clickedLetter)) {
+        [...currentWord].forEach((letter, index) => {
+            if(letter === clickedLetter) {
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed")
+            }
+        })
+    } else {
+        wrongGuessCount++;
+        hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+    }
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+}
+
+for (let i = 97; i <= 122; i++){
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+}
+
+getRandomWord();
